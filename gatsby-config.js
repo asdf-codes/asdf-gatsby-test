21c2c1d8308f687292f513cb2085d357aa401dcb
asdf-codes <kozakv@Vladimirs-iMac.local>
2020-05-11 12:28:37 -0400

    add gatsby mdx plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,20 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extension: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ]
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `asdf`,
