5903e76010b4b95d511591036ac034aa349e8e6a
asdf-codes <kozakv@Vladimirs-iMac.local>
2020-05-11 17:10:12 -0400

    added nav

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { useStaticQuery, Link, graphql } from 'gatsby'
+import { NavWrapper } from "../elements"
+
+export const Nav = () => {
+    
+    const data = useStaticQuery(graphql`
+        query {
+            logl: file(relativePath: {eq: "logl.svg"}) {
+                publicURL
+            }
+        }
+    `)
+    
+    return(
+        <NavWrapper>
+            <Link to="/">
+                <img 
+                    src={data.logl.publicURL} 
+                    alt="My logo"
+                    style={{
+                        margin: `0 auto`,
+                        maxWidth: '5rem',
+                        padding: `1.45rem 1.0875rem`,
+                      }}/>
+            </Link>
+        </NavWrapper>
+    )
+}
