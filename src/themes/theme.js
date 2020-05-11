f4f618549bf130b1e37702cc74cd226a2f60741a
asdf-codes <kozakv@Vladimirs-iMac.local>
2020-05-11 15:03:54 -0400

    add styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,35 @@
+export default {
+    fonts: {
+        main: "Multi, sans-serif",
+        code: "Roboto Mono, monospace",
+    },
+    colors: {
+        main1: "hsl(207, 70%, 59%)",
+        main2: "hsl(207, 70%, 59%)",
+        dark2: "hsl(227, 2%, 12%)",
+        dark1: "hsl(227, 2%, 26%)",
+        dark3: "hsl(227, 2%, 64%)",
+        light1: "hsl(0, 0%, 97%)",
+        light2: "hsl(0, 0%, 99%)",
+    },
+    breakpoints: {
+        mobile: "only screen and (mac-width: 50rem)",
+        tablet: "only screen and (mac-width: 65rem)",
+    },
+    spacing: {
+        xxSmall: ".25rem",
+        xSmall: ".5rem",
+        small: "1rem",
+        medium: "2rem",
+        large: "3rem",
+        xLarge: "4rem",
+        xxLarge: "6rem",
+    },
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link:"color 0.2 ease"
+    }, 
+    shadows: {
+        shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+    }
+}
