7e78fb9833fdfa7d40b959fed294d69a9a009a42
asdf-codes <kozakv@Vladimirs-iMac.local>
2020-05-11 15:26:47 -0400

    add theme provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,18 @@
+import React from 'react'
+import { ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme"
+
+export const wrapRootElement = ({element}) => (
+    <ThemeProvider theme={Theme}>
+    {element}
+    </ThemeProvider>    
+)
+   
+
+
+
+
+
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
