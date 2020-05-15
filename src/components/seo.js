ea0f7e872729d4c0095b520f01ce46f08e8385fc
asdf-codes <voroko@protonmail.com>
2020-05-15 11:26:11 -0400

    lowercase s? really?

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import {Helmet} from "react-helmet"
 import {StaticQuery, graphql} from "gatsby"
@@ -97,6 +90,14 @@ const detailsQuery = graphql`
         }
     }
 `
+
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
 
 
 // import React from "react"
