939e0c5d192fd7afd007ee24d7e2cbdacb470ce6
asdf-codes <oo@noadjective.com>
2020-05-18 00:05:22 -0400

    hellop2

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,7 @@ exports.createPages = async function({actions, graphql}) {
 
 // create paginated pages for posts
 
-    const postPerPage = 3
+    const postPerPage = 1
 
     const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
 
